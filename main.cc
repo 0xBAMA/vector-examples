2aa39b6030f63d6a7023ae4628d97b13fd5fd495
jb <jon.baker1222@gmail.com>
2020-03-09 19:16:12 -0400

    string cast

--- main.cc
+++ main.cc
@@ -2,16 +2,22 @@
 #define GLM_FORCE_SWIZZLE
 #define GLM_SWIZZLE_XYZW
 #include "glm/glm.hpp" //general vector types
+#include "glm/ext.hpp" //string cast
+#include "glm/gtx/string_cast.hpp"
 #include "glm/gtc/matrix_transform.hpp" // for glm::ortho
 #include "glm/gtc/type_ptr.hpp" //to send matricies gpu-side
 #include "glm/gtx/transform.hpp"
+
+#include <iostream>
 
 using namespace glm;
 
 int main(int argc, char const *argv[])
 {
   vec3 vec;
-  mat3 mat;
+  mat3 mat = mat3(1.0f);
+
+  std::cout<<glm::to_string(mat)<<std::endl;
 
   return 0;
 }
