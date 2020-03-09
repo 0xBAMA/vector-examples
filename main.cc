4fe32fcd9c76eec8ddcb532a94512e66ec3583ce
jb <jon.baker1222@gmail.com>
2020-03-09 19:38:23 -0400

    checkin

--- main.cc
+++ main.cc
@@ -9,18 +9,39 @@
 #include "glm/gtx/transform.hpp"
 
 #include <iostream>
+using std::endl;
+using std::cout;
 
 //usage details here: https://glm.g-truc.net/0.9.9/index.html
 
 
 using namespace glm;
 
+
+// std::cout<<glm::to_string(mat)<<std::endl; //this works, but it's ugly
+
+void print_matrix(mat3 mat)
+{
+  cout << mat[0][0] << " " << mat[1][0] << " " << mat[2][0] << endl;
+  cout << mat[0][1] << " " << mat[1][1] << " " << mat[2][1] << endl;
+  cout << mat[0][2] << " " << mat[1][2] << " " << mat[2][2] << endl;
+  cout << endl;
+}
+
+
 int main(int argc, char const *argv[])
 {
   vec3 vec;
   mat3 mat = mat3(1.0f);    //initialize with identity
 
-  std::cout<<glm::to_string(mat)<<std::endl;
+  print_matrix(mat);
+
+
+  //reference is done with mat[column][row]
+  mat[0][0] = 5.0f;
+  mat[0][1] = 5.0f;
+
+  print_matrix(mat);
 
   return 0;
 }
