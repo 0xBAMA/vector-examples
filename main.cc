d46c524be0c6ef6f25f8a65f0e1cb35d76af6ae1
jb <jon.baker1222@gmail.com>
2020-03-09 15:37:18 -0400

    checkin

---
+++ main.cc
@@ -0,0 +1,17 @@
+//vector math library GLM
+#define GLM_FORCE_SWIZZLE
+#define GLM_SWIZZLE_XYZW
+#include "glm/glm.hpp" //general vector types
+#include "glm/gtc/matrix_transform.hpp" // for glm::ortho
+#include "glm/gtc/type_ptr.hpp" //to send matricies gpu-side
+#include "glm/gtx/transform.hpp"
+
+using namespace glm;
+
+int main(int argc, char const *argv[])
+{
+  vec3 vec;
+  mat3 mat;
+
+  return 0;
+}
