abf2d408b42dc4089d3d66358178c416074ab1c9
jb <jon.baker1222@gmail.com>
2020-03-10 15:24:46 -0400

    formatting

--- includes.h
+++ includes.h
@@ -9,9 +9,12 @@
 #include "glm/gtx/transform.hpp"
 
 #include <iostream>
+#include <iomanip>
+
 using std::endl;
 using std::cout;
 
+using std::setw;
 
 using namespace glm;
 //usage details here: https://glm.g-truc.net/0.9.9/index.html
@@ -21,8 +24,8 @@ using namespace glm;
 void print_matrix(mat3 mat)
 {
   // std::cout<<glm::to_string(mat)<<std::endl; //this works, but it's ugly
-  cout << mat[0][0] << " " << mat[1][0] << " " << mat[2][0] << endl;
-  cout << mat[0][1] << " " << mat[1][1] << " " << mat[2][1] << endl;
-  cout << mat[0][2] << " " << mat[1][2] << " " << mat[2][2] << endl;
+  cout << setw(5) << mat[0][0] << " " << setw(5) << mat[1][0] << " " << setw(5) << mat[2][0] << endl;
+  cout << setw(5) << mat[0][1] << " " << setw(5) << mat[1][1] << " " << setw(5) << mat[2][1] << endl;
+  cout << setw(5) << mat[0][2] << " " << setw(5) << mat[1][2] << " " << setw(5) << mat[2][2] << endl;
   cout << endl;
 }
--- main.cc
+++ main.cc
@@ -5,13 +5,25 @@ int main(int argc, char const *argv[])
   vec3 vec;
   mat3 mat = mat3(1.0f);    //initialize with identity
 
-  print_matrix(mat);
+
+  cout << "Identity matrix:" << endl << endl;
+  print_matrix(mat);        //print the identity matrix
 
   //reference is done with mat[column][row]
+  // here just setting first column, first and second row values to 5
   mat[0][0] = 5.0f;
   mat[0][1] = 5.0f;
 
-  print_matrix(mat);
+  cout << "Change some values:" << endl << endl;
+  print_matrix(mat);          //print the matrix with the changes
+
+  mat3 matinv = inverse(mat); //compute the inverse of mat
+
+  cout << "Compute the inverse:" << endl << endl;
+  print_matrix(matinv);       //print the inverse of mat
+
+  cout << "Confirm the matrix times its inverse is in fact the identity matrix:" << endl << endl;
+  print_matrix(mat*matinv);   //confirm that mat*mat^-1 = I
 
   return 0;
 }
