e4c72b3c30c01df9a6f710e05bd792685d444023
jb <jon.baker1222@gmail.com>
2020-03-10 16:18:28 -0400

    define to clean up output code

--- includes.h
+++ includes.h
@@ -22,13 +22,16 @@ using namespace glm;
 //usage details here: https://glm.g-truc.net/0.9.9/index.html
 
 
+#define FMT setw(5) << std::setprecision(2) << std::fixed //less mess
+
+
 
 void print_matrix(mat3 mat)
 {
   // std::cout<<glm::to_string(mat)<<std::endl; //this works, but it's ugly
-  cout << setw(5) << std::setprecision(2) << std::fixed << mat[0][0] << " " << setw(5) << std::setprecision(2) << std::fixed << mat[1][0] << " " << setw(5) << std::setprecision(2) << std::fixed << mat[2][0] << endl;
-  cout << setw(5) << std::setprecision(2) << std::fixed << mat[0][1] << " " << setw(5) << std::setprecision(2) << std::fixed << mat[1][1] << " " << setw(5) << std::setprecision(2) << std::fixed << mat[2][1] << endl;
-  cout << setw(5) << std::setprecision(2) << std::fixed << mat[0][2] << " " << setw(5) << std::setprecision(2) << std::fixed << mat[1][2] << " " << setw(5) << std::setprecision(2) << std::fixed << mat[2][2] << endl;
+  cout << FMT << mat[0][0] << " " << FMT << mat[1][0] << " " << FMT << mat[2][0] << endl;
+  cout << FMT << mat[0][1] << " " << FMT << mat[1][1] << " " << FMT << mat[2][1] << endl;
+  cout << FMT << mat[0][2] << " " << FMT << mat[1][2] << " " << FMT << mat[2][2] << endl;
   cout << endl;
 }
 
@@ -36,9 +39,9 @@ vec3 print_matrix_times_vector(mat3 mat, vec3 vec)
 {
   vec3 temp = mat*vec;
 
-  cout << setw(5) << std::setprecision(2) << std::fixed << mat[0][0] << " " << setw(5) << std::setprecision(2) << std::fixed << mat[1][0] << " " << setw(5) << std::setprecision(2) << std::fixed << mat[2][0] << "          " << setw(5) << std::setprecision(2) << std::fixed << vec[0] << "        " << setw(5) << std::setprecision(2) << std::fixed << temp[0] << endl;
-  cout << setw(5) << std::setprecision(2) << std::fixed << mat[0][1] << " " << setw(5) << std::setprecision(2) << std::fixed << mat[1][1] << " " << setw(5) << std::setprecision(2) << std::fixed << mat[2][1] << "     *    " << setw(5) << std::setprecision(2) << std::fixed << vec[1] << "    =   " << setw(5) << std::setprecision(2) << std::fixed << temp[1] << endl;
-  cout << setw(5) << std::setprecision(2) << std::fixed << mat[0][2] << " " << setw(5) << std::setprecision(2) << std::fixed << mat[1][2] << " " << setw(5) << std::setprecision(2) << std::fixed << mat[2][2] << "          " << setw(5) << std::setprecision(2) << std::fixed << vec[2] << "        " << setw(5) << std::setprecision(2) << std::fixed << temp[2] << endl;
+  cout << FMT << mat[0][0] << " " << FMT << mat[1][0] << " " << FMT << mat[2][0] << "          " << FMT << vec[0] << "        " << FMT << temp[0] << endl;
+  cout << FMT << mat[0][1] << " " << FMT << mat[1][1] << " " << FMT << mat[2][1] << "     *    " << FMT << vec[1] << "    =   " << FMT << temp[1] << endl;
+  cout << FMT << mat[0][2] << " " << FMT << mat[1][2] << " " << FMT << mat[2][2] << "          " << FMT << vec[2] << "        " << FMT << temp[2] << endl;
   cout << endl;
 
   return temp;
