94b30b4b4f92aea8e61aa0e76cb7ca50d778fe44
jb <jon.baker1222@gmail.com>
2020-03-16 15:24:29 -0400

    checkin

--- includes.h
+++ includes.h
@@ -1,4 +1,5 @@
 //vector math library GLM
+//usage details here: https://glm.g-truc.net/0.9.9/index.html
 #define GLM_FORCE_SWIZZLE
 #define GLM_SWIZZLE_XYZW
 #include "glm/glm.hpp" //general vector types
@@ -18,16 +19,19 @@
 using std::endl;
 using std::cout;
 
+//output formatting
 using std::setw;
-
-//so you don't have to do 'glm::vec3' and 'glm::mat3' all over the place
-using namespace glm;
-//usage details here: https://glm.g-truc.net/0.9.9/index.html
-
-
 #define FMT setw(5) << std::setprecision(2) << std::fixed   //less mess
 
+//so you don't have to do 'glm::vec3' and 'glm::mat3' all over the place
+//note that there are quite a few other types, as well
+using glm::vec3;
+using glm::mat3;
 
+
+
+//I wrote these two functions so you can see formatted math operations, the way you'd write them symbolically
+//  that being said - if you wanted to do other operations, you'd have to define similar functions
 
 void print_matrix(mat3 mat)
 {
@@ -40,7 +44,7 @@ void print_matrix(mat3 mat)
 
 vec3 print_matrix_times_vector(mat3 mat, vec3 vec)
 {
-  vec3 temp = mat*vec;
+  vec3 temp = mat*vec;   //this is as hard as matrix multiplication is
 
   cout << FMT << mat[0][0] << " " << FMT << mat[1][0] << " " << FMT << mat[2][0] << "          " << FMT << vec[0] << "        " << FMT << temp[0] << endl;
   cout << FMT << mat[0][1] << " " << FMT << mat[1][1] << " " << FMT << mat[2][1] << "     *    " << FMT << vec[1] << "    =   " << FMT << temp[1] << endl;
--- main.cc
+++ main.cc
@@ -3,55 +3,65 @@
 int main(int argc, char const *argv[])
 {
 
-  mat3 mat = mat3(1.0f);    //initialize with identity
+  //initialize with identity
+  mat3 mat = mat3(1.0f);
 
+
+  //print the identity matrix
   cout << endl << "Identity matrix:" << endl << endl;
-  print_matrix(mat);        //print the identity matrix
+  print_matrix(mat);
 
 
-
-
+  //this is the std::random setup
 
   std::random_device rd;  //Will be used to obtain a seed for the random number engine
   std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
   std::uniform_int_distribution<> dis(1, 15);
 
-  // dis(gen) now gives a random number between 1 and 15
+  // dis(gen) now gives a random number between 1 and 15 - try different numbers if you want
   for (int col = 0; col < 3; col++)
     for (int row = 0; row < 3; row++)
       mat[col][row] = dis(gen); //reference is done with mat[column][row]
 
 
 
-
-
-
-
-
-
+  //print the matrix with the random changes
   cout << "Change some values:" << endl << endl;
-  print_matrix(mat);          //print the matrix with the changes
+  print_matrix(mat);
 
-  mat3 matinv = inverse(mat); //compute the inverse of mat
 
+  //compute the inverse of mat
+  mat3 matinv = inverse(mat);
+
+
+  //print the inverse of mat
   cout << "Compute the inverse:" << endl << endl;
-  print_matrix(matinv);       //print the inverse of mat
+  print_matrix(matinv);
 
+
+  //confirm that mat*mat^-1 = I
   cout << "Confirm the matrix times its inverse is in fact the identity matrix:" << endl << endl;
-  print_matrix(mat*matinv);   //confirm that mat*mat^-1 = I
+  print_matrix(mat*matinv);
+
 
   cout << endl << endl << endl;
-
   cout << "Matrix times a vector:" << endl << endl;
 
+
+  //some random vector to use
   vec3 vec; vec[0] = 1; vec[1] = 2; vec[2] = 3;
 
+
+  //multiply that by your randomly generated matrix - the function returns the result of the multiplication
   vec3 result_vec = print_matrix_times_vector(mat, vec);
 
-  cout << "and the result of that, times the inverse matrix:" << endl << endl;
 
+  //this is just illustrating a basic property of inverses - the same way that n*(1/n) = 1
+  cout << "and the result of that, times the inverse matrix:" << endl << endl;
   print_matrix_times_vector(matinv, result_vec);
 
+
+  //you should see the vector the same as it was initialized
   cout << "gives that vector back." << endl << endl;
 
   return 0;
